75ffb50f2e234a7ea76fe3c778ee4a38f8b3fca1
Santiago Aristi <santiago.aristi@mykenzie.snhu.edu>
2022-09-23 12:31:55 -0400

    second commit Main.java and SpeedConverter.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args){
+        SpeedConverter.printConversion(2.65);
+        SpeedConverter.printConversion(22.50);
+        SpeedConverter.printConversion(56.15);
+        SpeedConverter.printConversion(72.75);
+        SpeedConverter.printConversion(102.90);
+    }
+}
